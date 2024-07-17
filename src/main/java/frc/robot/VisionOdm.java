e502cf3a86c0d7f9a168a9367211b0ec84c3f085
Jdorme <127994081+Jdorme@users.noreply.github.com>
2024-07-17 12:31:29 -0500

    Update VisionOdm.java
    incorporated Metatag 2

--- src/main/java/frc/robot/VisionOdm.java
+++ src/main/java/frc/robot/VisionOdm.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.math.VecBuilder;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /** Add your docs here. */
 public class VisionOdm {
 
@@ -16,11 +16,14 @@ public class VisionOdm {
     public void updateodm(){ 
     
     
-      boolean doRejectUpdate = false;
+     boolean doRejectUpdate = false;
      boolean equalNull;
      CommandSwerveDrivetrain drivetrain = RobotContainer.drivetrain;
-     
-    
+     boolean meta2 = true;
+     double AngularVelocity = drivetrain.getPigeon2().getAngularVelocityZWorld().getValueAsDouble();
+     SmartDashboard.putNumber("megatagDegrees",AngularVelocity);
+     double rotation = drivetrain.getPigeon2().getRotation2d().getDegrees();
+    if (meta2 == false){
      LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-farmone");
        if(mt1.tagCount == 1 && mt1.rawFiducials.length == 1)
       {
@@ -44,5 +47,22 @@ public class VisionOdm {
         drivetrain.addVisionMeasurement(
             mt1.pose,
             mt1.timestampSeconds);
+      }} else{ 
+      LimelightHelpers.SetRobotOrientation("limelight-farmone", rotation, 0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-farmone");
+      if(Math.abs(AngularVelocity) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      {
+        doRejectUpdate = true;
       }
+      if(mt2.tagCount == 0)
+      {
+        doRejectUpdate = true;
+      }
+      if(!doRejectUpdate)
+      {
+        drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+        drivetrain.addVisionMeasurement(
+            mt2.pose,
+            mt2.timestampSeconds);
+      }}
 }}
