0bcc8ec8101f7f3891713d3e296be39057c265a8
Jdorme <127994081+Jdorme@users.noreply.github.com>
2024-07-18 14:21:45 -0500

    Update VisionOdm.java
    Added Support for 2 Limelights
    Added Support for limelight disconnect so if a limelight disconnects during a match it doesn't just crash the code.

--- src/main/java/frc/robot/VisionOdm.java
+++ src/main/java/frc/robot/VisionOdm.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.LimelightHelpers.PoseEstimate;
 /** Add your docs here. */
 public class VisionOdm {
 
@@ -16,53 +17,109 @@ public class VisionOdm {
     public void updateodm(){ 
     
     
-     boolean doRejectUpdate = false;
+     boolean farmonedoRejectUpdate = false;
+     boolean farmtwodoRejectUpdate = false;
      boolean equalNull;
+     PoseEstimate bestPose;
+     PoseEstimate farmone;
+     PoseEstimate farmtwo;
      CommandSwerveDrivetrain drivetrain = RobotContainer.drivetrain;
-     boolean meta2 = true;
+     boolean meta2 = false;
      double AngularVelocity = drivetrain.getPigeon2().getAngularVelocityZWorld().getValueAsDouble();
      SmartDashboard.putNumber("megatagDegrees",AngularVelocity);
      double rotation = drivetrain.getPigeon2().getRotation2d().getDegrees();
     if (meta2 == false){
-     LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-farmone");
-       if(mt1.tagCount == 1 && mt1.rawFiducials.length == 1)
+     try {
+       farmone = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-farmone");
+       } catch (Exception e) {
+        farmone = null;
+       } 
+     try {
+       farmtwo = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-farmtwo");
+       } catch (Exception e) {
+        farmtwo = null;
+       }
+     //Limelight farmone process to reject bad pose estimites and keep the good ones 
+     if (farmone !=null){
+      SmartDashboard.putString("Limelight1 Status", "ready");
+     if(farmone.tagCount == 1 && farmone.rawFiducials.length == 1)
       {
-        if(mt1.rawFiducials[0].ambiguity > .7)
+        if(farmone.rawFiducials[0].ambiguity > .7)
         {
-          doRejectUpdate = true;
+          farmonedoRejectUpdate = true;
         }
-        if(mt1.rawFiducials[0].distToCamera > 3)
+        if(farmone.rawFiducials[0].distToCamera > 3)
         {
-          doRejectUpdate = true;
+          farmonedoRejectUpdate = true;
         }
       }
-      if(mt1.tagCount == 0)
+      if(farmone.tagCount == 0)
       {
-        doRejectUpdate = true;
+        farmonedoRejectUpdate = true;
+      }}else{
+        farmonedoRejectUpdate=true;
+        SmartDashboard.putString("Limelight1 Status", "Disconnected");
+    }
+     //Limelight farmtwo process to reject bad pose estimites and keep the good ones
+     if (farmtwo !=null){ 
+      SmartDashboard.putString("Limelight2 Status", "ready");
+     if(farmtwo.tagCount == 1 && farmtwo.rawFiducials.length == 1)
+      {
+        if(farmtwo.rawFiducials[0].ambiguity > .7)
+        {
+          farmtwodoRejectUpdate = true;
+        }
+        if(farmtwo.rawFiducials[0].distToCamera > 3)
+        {
+          farmtwodoRejectUpdate = true;
+        }
       }
-
-      if(!doRejectUpdate)
+      if(farmtwo.tagCount == 0)
       {
+        farmtwodoRejectUpdate = true;
+      }}else{
+      farmtwodoRejectUpdate = true;
+      SmartDashboard.putString("Limelight2 Status", "Disconnected");
+      }
+      //This is where we decide what pose we want to use to update our odomatery so we check if both are vaild if so we check which has a higher tag to image area 
+      if (farmonedoRejectUpdate != true && farmtwodoRejectUpdate != true){
+        bestPose = (farmone.avgTagArea >= farmtwo.avgTagArea ) ? farmone : farmtwo;
+      }else if (farmonedoRejectUpdate != true){
+        bestPose = farmone;
+      }else if(farmtwodoRejectUpdate != true) {
+        bestPose = farmtwo;
+      }else{bestPose=null;}
+        
+      if (bestPose != null) {
         drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.5,.5,9999999));
         drivetrain.addVisionMeasurement(
-            mt1.pose,
-            mt1.timestampSeconds);
-      }} else{ 
-      LimelightHelpers.SetRobotOrientation("limelight-farmone", rotation, 0, 0, 0, 0, 0);
-      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-farmone");
-      if(Math.abs(AngularVelocity) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+            bestPose.pose,
+            bestPose.timestampSeconds);}
+      } else{ 
+       try {LimelightHelpers.SetRobotOrientation("limelight-farmone", rotation, 0, 0, 0, 0, 0);
+       farmone = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-farmone");
+        
+       } catch (Exception e) {
+        farmone = null;
+       } 
+      
+      if(farmone !=null){
+        SmartDashboard.putString("Limelight1 Status", "ready");
+       if(Math.abs(AngularVelocity) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
       {
-        doRejectUpdate = true;
+        farmonedoRejectUpdate = true;
       }
-      if(mt2.tagCount == 0)
+      if(farmone.tagCount == 0)
       {
-        doRejectUpdate = true;
+        farmonedoRejectUpdate = true;
       }
-      if(!doRejectUpdate)
+      if(!farmonedoRejectUpdate)
       {
         drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
         drivetrain.addVisionMeasurement(
-            mt2.pose,
-            mt2.timestampSeconds);
-      }}
-}}
+            farmone.pose,
+            farmone.timestampSeconds);
+      }}else{
+        SmartDashboard.putString("Limelight1 Status", "Disconnected");
+      }
+}}}
